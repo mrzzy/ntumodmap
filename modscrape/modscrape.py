d1c00a1335f26dccac0a258b58253d341726fd3a
lczm <zemingchua@gmail.com>
2023-10-01 23:02:12 +0800

    Formatting

--- modscrape/modscrape.py
+++ modscrape/modscrape.py
@@ -3,7 +3,6 @@
 # Modscrape Module Scraper
 #
 
-
 from typing import Dict, cast
 import requests
 from bs4 import BeautifulSoup, Tag
@@ -13,6 +12,7 @@ from parser import parse
 from pprint import pprint
 
 COURSE_CONTENT_URL = "https://wis.ntu.edu.sg/webexe/owa/aus_subj_cont"
+
 
 def extract_options(page: BeautifulSoup, name: str) -> Dict[str, str]:
     """Extract options from the select element with the given name attribute.
@@ -30,7 +30,9 @@ def extract_options(page: BeautifulSoup, name: str) -> Dict[str, str]:
 def scrape_modules(semester: str, course: str):
     # scrape modules
     year, term = semester.split("_")
-    with requests.post(f"{COURSE_CONTENT_URL}.main_display1", {
+    with requests.post(
+        f"{COURSE_CONTENT_URL}.main_display1",
+        {
             # for some reason, the client side post request sends 'acadsem'
             # we emulate that behavior here.
             "acadsem": [
@@ -42,8 +44,9 @@ def scrape_modules(semester: str, course: str):
             "boption": "CLoad",
             "acad": year,
             "semester": term,
-    }) as response:
-        mod_listing = BeautifulSoup(response.content.decode(),"lxml")
+        },
+    ) as response:
+        mod_listing = BeautifulSoup(response.content.decode(), "lxml")
         # each module is encoded as table
         mod_tables = mod_listing.find_all("table")
         # print(mod_tables)
@@ -51,24 +54,27 @@ def scrape_modules(semester: str, course: str):
         lines = []
         for table in mod_tables:
             individual = []
-            rows = table.find_all('tr')
+            rows = table.find_all("tr")
             for row in rows:
-                cols = [ele.text.strip() for ele in row.find_all('td')]
+                cols = [ele.text.strip() for ele in row.find_all("td")]
                 individual.append(cols)
             lines.append(individual)
         return lines
+
 
 # Takes a nested list and concatenates inner list
 # e.g. [[a], [b]] -> [[a, b]]
 def concat_nested(lines: list[list[str]]) -> list[list[str]]:
     return [list(chain(*line)) for line in lines]
 
+
 # Takes a nested list and filters out all empty strings
 # e.g. [[a, b, ''], [c, '']] -> [[a, b], [c]]
 def filter_empty(lines: list[list[str]]) -> list[list[str]]:
     return [list(filter(None, line)) for line in lines]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # scrape main page
     with requests.get(f"{COURSE_CONTENT_URL}.main") as response:
         # lxml parser is used to handle malformed html (eg. unclosed tags)
@@ -81,8 +87,11 @@ if __name__ == '__main__':
     lines = scrape_modules("2023_1", "CSC;;1;F")
     lines = concat_nested(lines)
     lines = filter_empty(lines)
-    lines = [' '.join(line) for line in lines]
+    lines = [" ".join(line) for line in lines]
+
+    pprint(lines)
 
     tokens = lex(lines)
+
     # pprint(tokens)
     parse(tokens)
