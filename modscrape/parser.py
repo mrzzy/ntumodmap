9dddf9b61dffe1cc6b1b81e9be5aa32be33a9336
Zhu Zhanyan <program.nom@gmail.com>
2023-10-29 23:12:22 +0800

    fixup! fix(modscrape): pre_requisite_year(): handle case when previous_token is none

--- modscrape/parser.py
+++ modscrape/parser.py
@@ -287,7 +287,7 @@ class Parser:
         # Note that I have not been able to find any nesting of years, i.e. Year 2 & Year 3 standing
         if self.match_identifier("Year") or self.match_identifier("Study"):
             # This moves past both Year and Study Year
-            previous_token = self.previous_token
+            previous_token = self.previous_token()
             if previous_token is not None and previous_token.literal == "Study":
                 self.move()
             year = self.consume(
