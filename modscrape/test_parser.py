e44560067bd8330c64886747c1cf0836e8291c98
Zhu Zhanyan <program.nom@gmail.com>
2023-11-09 07:44:14 +0800

    test(modscrape): add unit test for tokens_to_module()

--- modscrape/test_parser.py
+++ modscrape/test_parser.py
@@ -4,6 +4,41 @@
 # Parser
 #
 
-# TODO: demo test, replace with actual tests
-def test_demo():
-    pass
+from parser import tokens_to_module
+
+from module import Module, ModuleCode
+from tok import Token, TokenType
+
+
+def test_tokens_to_module():
+    for year in [None, Token(TokenType.NUMBER, "2")]:
+        code, title = ModuleCode("SC1005"), "Digital Logic"
+        module_mutually_exclusives = [ModuleCode("CZ1005")]
+        module_pre_requisite_mods = [[ModuleCode(f"SC100{i}") for i in [1, 2]]]
+        is_pass_fail = False
+
+        actual = tokens_to_module(
+            module_code=code,
+            module_title=Token(TokenType.IDENTIFIER, title),
+            module_au=Token(TokenType.AU, "3"),
+            module_mutually_exclusives=module_mutually_exclusives,
+            module_pre_requisite_year=year,
+            module_pre_requisite_mods=module_pre_requisite_mods,
+            module_pass_fail=is_pass_fail,
+        )
+
+        expected = Module(
+            code=code,
+            title=title,
+            au=3,
+            mutually_exclusives=module_mutually_exclusives,
+            needs_year=None if year is None else int(year.literal),
+            needs_modules=module_pre_requisite_mods,
+            rejects_courses=[],
+            allowed_courses=[],
+            rejects_modules=[],
+            is_bde=False,
+            is_pass_fail=is_pass_fail,
+        )
+
+        assert actual == expected
