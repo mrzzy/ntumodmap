62fab90940a8f4abd276cc9289ffc09812205143
Zhu Zhanyan <program.nom@gmail.com>
2023-12-10 17:14:21 +0800

    test(modscrape): scrape_modules() on core & minor mods

---
+++ modscrape/test_modscrape.py
@@ -0,0 +1,22 @@
+#
+# Modscrape
+# Module Scraper
+# Tests
+#
+
+
+from importlib.resources import read_text
+
+import test_resources
+from modscrape import scrape_modules
+
+
+def test_scrape_modules_core():
+    assert len(scrape_modules(read_text(test_resources, "cs_core_modules.html"))) == 41
+
+
+def test_scrape_modules_minor():
+    assert (
+        len(scrape_modules(read_text(test_resources, "art_hist_minor_modules.html")))
+        == 18
+    )
