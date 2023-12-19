9cea2f41f9caa2019901b02d752726aa4e0e0104
Zhu Zhanyan <program.nom@gmail.com>
2023-12-19 06:18:32 +0000

    refactor(modscrape): rename 'keywords' dict to 'token_types'
    to prevent confusion with new 'Keywords' type

--- modscrape/lexer.py
+++ modscrape/lexer.py
@@ -7,7 +7,7 @@ from typing import Iterable
 
 from tok import Token, TokenType
 
-keywords: dict[str, TokenType] = {
+token_types: dict[str, TokenType] = {
     "AU": TokenType.AU,
     "Grade": TokenType.GRADE,
     "Type": TokenType.TYPE,
@@ -71,9 +71,9 @@ def lex(lines: Iterable[str]) -> list[list[Token]]:
                 while extend < len(line) and line[extend].isalnum():
                     extend += 1
                 identifier = line[current - 1 : extend]
-                # check if its a keyword, otherwise its an identifier
-                if identifier in keywords:
-                    sub_tokens.append(Token(keywords[identifier], identifier))
+                # check if its a token type, otherwise its an identifier
+                if identifier in token_types:
+                    sub_tokens.append(Token(token_types[identifier], identifier))
                 elif identifier.isdigit():
                     sub_tokens.append(Token(TokenType.NUMBER, identifier))
                 else:
